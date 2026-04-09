6f0804d9f4bfa1e191920f34ab5e532893455019
‪Ahmed Fathallah <ahmedfathallaali461@outlook.com>
2026-04-09 19:38:40 +0200

    Add main JavaScript file and refactor overall project styles

---
+++ assets/js/login.js
@@ -0,0 +1,42 @@
+// Form Validation
+const loginForm = document.querySelector('form');
+// تصحيح: ضفنا # لأنهم ID
+const phonNumber = document.querySelector('#phonNumber'); 
+const password = document.querySelector('#password');
+const togglePass = document.querySelector('#togglePassword');
+
+loginForm.addEventListener('submit', function(event) {
+    const inputs = [phonNumber, password];
+    let hasError = false;
+
+    inputs.forEach(input => {
+        if (input.value.trim() === "") {
+            hasError = true;
+            
+            input.style.border = "2px solid #ff4d4d";
+            
+            input.classList.add('error-shake');
+            
+            setTimeout(() => {
+                input.classList.remove('error-shake');
+            }, 500);
+
+        } else {
+            input.style.border = "1px solid var(--text-muted)";
+        }
+    });
+
+    if (hasError) {
+        event.preventDefault(); // بيمنع إرسال البيانات لو فيه خانة فاضية
+    }
+});
+
+// Toggle Password
+
+togglePass.addEventListener('click', function (e){
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    const icon = this.querySelector('i');
+    icon.classList.toggle('fa-eye');
+    icon.classList.toggle('fa-eye-slash');
+});
